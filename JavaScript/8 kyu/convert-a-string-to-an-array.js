cd071f2714492cde713ff7a47f567530e7c7cdc4
Uladzimir Bachyla <uladzimirbachyla@MacBook-Pro-Uladzimir.local>
2018-04-14 02:05:10 +0300

    Add dates & Rename

---
+++ JavaScript/8 kyu/convert-a-string-to-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * Created on Sat Apr 14 2:01:23 2018
+ * @author: Uladzimir Bachyla
+ */
+
+ /**
+  * TODO: Write a function to split a string and convert it into an
+  * array of words. For example:
+  * "Robin Singh" ==> ["Robin", "Singh"]
+  */
+
+  function stringToArray(string) {
+      let res = strin.split(" ");
+  }
