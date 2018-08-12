51b5998ffad0271d2d4b34ec96c45e940294cb23
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2018-08-12 22:51:54 +0300

    Add dates & Rename

---
+++ JavaScript/4 kuy/matrix-determinant.js
@@ -0,0 +1,48 @@
+/**
+ * Created on Sun Aug 12 10:45:03 2018
+ * @author: Uladzimir Bachyla
+ */
+
+ /**
+  * Write a function that accepts a square matrix (n x n 2D array) and returns the determinant of the matrix.
+  * How to take the determinant of a matrix -- it is simplest to start with the smallest cases:
+  * A 1x1 matrix |a| has determinant a. A 2x2 matrix [[a, b], [c, d]] or
+  * |a b|
+  * |c d|
+  * has determinant ad - bc.
+  * The determinant of an n x n sized matrix is calculated by reducing the problem to the calculation of the determinants
+  *  of n n-1 x n-1 matrices. For the 3x3 case, [[a, b, c], [d, e, f], [g, h, i]] or
+  * |a b c|
+  * |d e f|
+  * |g h i|
+  * the determinant is: a * det(a_minor) - b * det(b_minor) + c * det(c_minor) where det(a_minor) refers to taking the
+  * determinant of the 2x2 matrix created by crossing out the row and column in which the element a occurs, or
+  * |e f|
+  * |h i|
+  * Note the alternation of signs.
+  * The determinant of larger matrices are calculated analogously, e.g. if M is a 4x4 matrix with first 
+  * row [a, b, c, d], det(M) = a * det(a_minor) - b * det(b_minor) + c * det(c_minor) - d * det(d_minor)
+  */
+
+ function determinant(m) {
+    if (m.length == 1)
+      return m[0][0];
+      function calc(matrix) {
+          if (matrix.length == 2)
+          return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
+          let res = 0, sign = 1;
+          for (let i = 0; i < matrix[0].length; i++) {
+              let temp = [];
+              for (let j = 1; j < matrix.length; j++) {
+                  temp.push([]);
+                  for (let k = 0; k < matrix[0].length; k++) {
+                      if (k != i) temp[j-1].push(matrix[j][k]);
+                    }
+                }
+                res += matrix[0][i] * sign * calc(temp.slice());
+                sign *= -1;
+            }
+            return res;
+        }
+        return calc(m);
+    };
