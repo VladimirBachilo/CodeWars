98593eda6c6d945330bec54f13fe256337010931
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2018-12-18 08:36:24 +0300

    Add dates & Rename

---
+++ JavaScript/8 kyu/keep-hydrated.js
@@ -0,0 +1,11 @@
+/**
+ * Created on Tue Dec 18 8:34:55 2018
+ * @author: Uladzimir Bachyla
+ */
+
+ /**
+  * TODO: You get given the time in hours and you need to return the number
+  *  of litres Nathan will drink, rounded to the smallest value.
+  */
+
+  const litres = time => Math.floor(time * 0.5);
