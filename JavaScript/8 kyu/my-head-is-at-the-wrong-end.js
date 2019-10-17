a5928bac720574a009ef133334784d0d5bb2d2d3
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2019-10-17 14:35:24 +0300

    Add dates & Rename

---
+++ JavaScript/8 kyu/my-head-is-at-the-wrong-end.js
@@ -0,0 +1,13 @@
+/**
+ * Created on Tue Nov 7 14:33:00 2019
+ * @author: Uladzimir Bachyla
+ */
+
+/**
+ * TODO: Save the animals by switching them back. You will be
+ * given an array which will have three values (tail, body,
+ * head). It is your job to re-arrange the array so that the
+ * animal is the right way round (head, body, tail).
+ */
+
+const fixTheMeerkat = arr => arr.reverse();
